b161c846d5e86d4c7d065845f67c0070347e4aa5
Hieuvm1903 <75627361+Hieuvm1903@users.noreply.github.com>
2023-05-24 01:57:40 +0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,10 +1,14 @@
 
 
+import folium
 
 import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime
+
+from streamlit_folium import st_folium, folium_static
+
 st.title('Accidents in UK')
 
 DATE_COLUMN = 'date'
@@ -20,12 +24,16 @@ def load_data():
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
-
 data_load_state = st.text('Loading data...')
 data = load_data()
+data = data.dropna(how='any',axis=0) 
 data_load_state.text("Done!")
+dataset = data
+City_of_London = dataset[dataset['local_authority_highway'] == 'E09000001']
+Westminster = dataset[dataset['local_authority_highway'] == 'E09000033']
 
-vis = data.loc[:,['date/time','time','longitude','latitude']]
+data = pd.concat([City_of_London, Westminster], axis = 0)
+vis = data.loc[:,['date','time','longitude','latitude']]
 vis['time']= pd.to_datetime(vis['time'],format= '%H:%M' )
 
 #vis.set_index(['date/time'], inplace = True)
@@ -34,24 +42,65 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data2)
 
+from geopy.distance import great_circle
+
+def greatcircle(x,y):
+    lat1, long1 = x[0], x[1]
+    lat2, long2 = y[0], y[1]
+    dist = great_circle((lat1,long1),(lat2,long2)).meters
+    return dist
+
+from sklearn.cluster import DBSCAN as dbscan
 
 
 
-# Using "with" notation
 with st.sidebar:
     
     date1 = st.date_input("from",key = "start",min_value  = datetime.date(2012, 1, 1), max_value = datetime.date.today(), value =  datetime.date(2012, 1, 1) )
     date2 = st.date_input("to",key = "finish",min_value  = date1, max_value = datetime.date.today())
-    hour_to_filter = st.slider('hour from', 0, 23, 17, key = 2)
-    hour_end = st.slider('hour to', 0, 24, 18,key = 1)
+    if st.checkbox('Add time element'):
+        hour_to_filter = st.slider('hour from', 0, 23, 0, key = 2)
+        hour_end = st.slider('hour to', 0, 24, 24,key = 1)
+    else:
+        hour_to_filter, hour_end = [0,24]
 # Some number in the range 0-23
 
 filtered_data = data2[(data2['time'].dt.hour >= hour_to_filter) & (data2['time'].dt.hour < hour_end)
-&(data['date'].dt.date >=date1) & (data['date'].dt.date <= date2)]
+&(data2['date'].dt.date >=date1) & (data2['date'].dt.date <= date2)]
 
 st.subheader('Map of all accidents from %s:00 to %s:00' % (hour_to_filter, hour_end))
 st.map(filtered_data)
-
 # Using object notation
 st.write(len(filtered_data.index))
 
+max_cluster_distance = 100
+min_samples_in_cluster = 5
+
+location_data = dataset[['latitude','longitude']]
+
+clusters = dbscan(eps = max_cluster_distance, min_samples = min_samples_in_cluster, metric=greatcircle).fit(location_data)
+
+labels = clusters.labels_
+unique_labels = np.unique(clusters.labels_)
+
+dataset['Cluster'] = labels
+# Using "with" notation
+location = dataset['latitude'].mean(), dataset['longitude'].mean()
+
+map_plot = folium.Map(location=location,zoom_start=13)
+clust_colours = ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928']
+fg = folium.FeatureGroup(name= 'Marker')
+
+for i in range(0,len(dataset)):
+    colouridx = dataset['Cluster'].iloc[i]
+    if colouridx == -1:
+        pass
+    else:
+        col = clust_colours[colouridx%len(clust_colours)]
+        fg.add_child(       folium.Marker([dataset['latitude'].iloc[i],dataset['longitude'].iloc[i]]).add_to(map_plot))
+    height=500,
+st_folium(map_plot,
+    feature_group_to_add=fg,
+    width=1200,
+    height=500,)
+st.write(len(dataset.index))
